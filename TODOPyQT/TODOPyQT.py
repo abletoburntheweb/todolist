8d770c8974345604d2b88a3d5f5302d8eb866e8a
abletoburntheweb <115485262+abletoburntheweb@users.noreply.github.com>
2024-04-06 10:20:32 +0300

    add checkboxes

--- TODOPyQT/TODOPyQT.py
+++ TODOPyQT/TODOPyQT.py
@@ -1,6 +1,6 @@
 from random import randint
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QPushButton, QCheckBox
 from PyQt5.QtGui import QPainter, QColor
 
 
@@ -19,10 +19,6 @@ class Rect(QGraphicsView):
         self.setVerticalScrollBarPolicy(1)  # Отключаем вертикальный скроллбар
 
 
-
-
-
-
 class MainWin(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -38,6 +34,25 @@ class MainWin(QMainWindow):
 
     def main_screen(self):
         self.clear_window()
+        self.setFixedSize(500, 700)
+
+        checkbox1 = QCheckBox(self)
+        checkbox1.setGeometry(5, 105, 20, 20)
+
+        delete_btn1 = QPushButton('❌', self)
+        delete_btn1.setGeometry(470, 105, 20, 20)
+
+        checkbox2 =     QCheckBox(self)
+        checkbox2.setGeometry(5, 155, 20, 20)
+
+        delete_btn2 = QPushButton('❌', self)
+        delete_btn2.setGeometry(470, 155, 20, 20)
+
+        checkbox3 = QCheckBox(self)
+        checkbox3.setGeometry(5, 205, 20, 20)
+
+        delete_btn3 = QPushButton('❌', self)
+        delete_btn3.setGeometry(470, 205, 20, 20)
 
         self.text1 = QtWidgets.QLabel("HIGH", self)
         self.text1.move(220, 25)
@@ -47,25 +62,29 @@ class MainWin(QMainWindow):
         self.btnTs1 = QtWidgets.QPushButton('Добавить важных дел', self)
         self.btnTs1.setGeometry(25, 100, 450, 40)
         self.btnTs1.setStyleSheet(
-            "QPushButton { border-radius: 15px; background-color: white; color: #BBBBBB; font-size: 20px}")
+            "QPushButton { border-radius: 15px; background-color: white; color: #BBBBBB;border :1px solid; "
+            "border-color: #989898; font-size: 20px}")
         self.btnTs1.show()
 
         self.btnTs1 = QtWidgets.QPushButton('Помыть посуду', self)
         self.btnTs1.setGeometry(25, 150, 450, 40)
         self.btnTs1.setStyleSheet(
-            "QPushButton { border-radius: 15px; background-color: white; color: #202020; font-size: 20px}")
+            "QPushButton { border-radius: 15px; background-color: white; color: #202020;border :1px solid; "
+            "border-color: #989898; font-size: 20px}")
         self.btnTs1.show()
 
         self.btnTs2 = QtWidgets.QPushButton('Сверстать этот TODO list', self)
         self.btnTs2.setGeometry(25, 200, 450, 40)
         self.btnTs2.setStyleSheet(
-            "QPushButton { border-radius: 15px; background-color: white; color: #202020; font-size: 20px}")
+            "QPushButton { border-radius: 15px; background-color: white; color: #202020;border :1px solid; "
+            "border-color: #989898; font-size: 20px}")
         self.btnTs2.show()
 
         self.btnTs3 = QtWidgets.QPushButton('Начать делать задачу', self)
         self.btnTs3.setGeometry(25, 250, 450, 40)
         self.btnTs3.setStyleSheet(
-            "QPushButton { border-radius: 15px; background-color: white; color: #202020; font-size: 20px}")
+            "QPushButton { border-radius: 15px; background-color: white; color: #202020; border :1px solid; "
+            "border-color: #989898; font-size: 20px}")
 
         self.btnTs4 = QtWidgets.QPushButton('⚙', self)
         self.btnTs4.setGeometry(470, 10, 20, 21)
@@ -80,12 +99,14 @@ class MainWin(QMainWindow):
         self.btnTs4 = QtWidgets.QPushButton('Добавить дел', self)
         self.btnTs4.setGeometry(25, 400, 450, 40)
         self.btnTs4.setStyleSheet(
-            "QPushButton { border-radius: 15px; background-color: white; color: #BBBBBB; font-size: 20px}")
+            "QPushButton { border-radius: 15px; background-color: white; color: #BBBBBB; border :1px solid; "
+            "border-color: #989898; font-size: 20px}")
 
         self.btnTs5 = QtWidgets.QPushButton('Записаться к стоматологу', self)
         self.btnTs5.setGeometry(25, 450, 450, 40)
         self.btnTs5.setStyleSheet(
-            "QPushButton { border-radius: 15px; background-color: white; color: #202020; font-size: 20px}")
+            "QPushButton { border-radius: 15px; background-color: white; color: #202020; border :1px solid; "
+            "border-color: #989898; font-size: 20px}")
 
     def second_screen(self):
         print('Settings menu opened')
