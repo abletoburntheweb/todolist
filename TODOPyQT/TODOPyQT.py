6d6163215dc305bddf89ce57d1a8ceefa0809186
abletoburntheweb <115485262+abletoburntheweb@users.noreply.github.com>
2024-05-23 22:45:52 +0300

    add days of the week

--- TODOPyQT/TODOPyQT.py
+++ TODOPyQT/TODOPyQT.py
@@ -3,20 +3,21 @@ import json
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QPixmap, QPalette, QBrush
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QInputDialog, QCheckBox, QMessageBox, \
-    QLineEdit, QListWidget, QTextEdit, QListWidgetItem, QComboBox, QScrollArea, QScrollBar, QVBoxLayout, QWidget, \
-    QLayout
+    QLineEdit, QListWidget, QTextEdit, QListWidgetItem, QComboBox
 from PyQt5.QtCore import Qt
-from weekend import weekend_dropdown
 from note_page import NotePage
 from ui_elements import setup_ui_elements
+
 
 class MainWin(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle('TODO List')
         self.setGeometry(750, 250, 500, 700)
         self._note_page = NotePage(self)
+        self.buttons = []
+
+        self.setup_main_buttons()
         self.load_tasks()
 
         self.current_background_image = "background1.png"
@@ -42,23 +43,16 @@ class MainWin(QMainWindow):
             notes_data = json.load(file)
             self.notes = notes_data.get("notes", {})
 
-        self.main_screen()
 
     def save_tasks_to_file(self):
-        tasks_data = {
-            "important_tasks": self.important_tasks,
-            "additional_tasks": self.additional_tasks,
-            "tasks_high_priority": self.tasks_high_priority,
-            "tasks_low_priority": self.tasks_low_priority
-        }
         with open("tasks.json", "w", encoding="utf-8") as file:
-            json.dump(tasks_data, file, ensure_ascii=False)
+            json.dump(self.tasks_data, file, ensure_ascii=False, indent=4)
 
     def save_notes_to_file(self):
         title = self.note_title_edit.text()
         notes = self.notes_text_edit.toPlainText()
         if title:
-            if title in self.notes:  # Check if the note exists before deleting it
+            if title in self.notes:
                 del self.notes[title]
                 try:
                     with open("notes.json", "w", encoding="utf-8") as file:
@@ -69,7 +63,7 @@ class MainWin(QMainWindow):
                                         f"An error occurred while saving the note: {str(e)}")
             else:
                 QMessageBox.warning(self, 'Ошибка', 'Заметка не существует')
-            self.clear_note_page()  # Clear the note page after deleting the note
+            self.clear_note_page()
         else:
             QMessageBox.warning(self, 'Внимание', 'Заголовок не может быть пустым.')
 
@@ -107,40 +101,121 @@ class MainWin(QMainWindow):
     def load_tasks(self):
         try:
             with open("tasks.json", "r", encoding="utf-8") as file:
-                tasks_data = json.load(file)
-                self.important_tasks = tasks_data.get("important_tasks", [])
-                self.tasks_high_priority = tasks_data.get("tasks_high_priority", [])
-                self.additional_tasks = tasks_data.get("additional_tasks", [])
-                self.tasks_low_priority = tasks_data.get("tasks_low_priority", [])
+                self.tasks_data = json.load(file)
         except FileNotFoundError:
-            self.important_tasks = []
-            self.tasks_high_priority = []
-            self.additional_tasks = []
-            self.tasks_low_priority = []
+            self.tasks_data = {str(i): {
+                "important_tasks": [],
+                "additional_tasks": [],
+                "tasks_high_priority": [],
+                "tasks_low_priority": []
+            } for i in range(1, 8)}
 
-    def main_screen(self):
-        self.clear_window()
+    def setup_main_buttons(self):
+        button_width = 60
+        button_height = 40
+        button_spacing = 10
+        start_x = 25
+        start_y = 10
+
+        self.buttons = []
+        for i in range(1, 8):
+            btn = QPushButton(f"{i}", self)
+
+            btn.setGeometry(start_x + (i - 1) * (button_width + button_spacing), start_y, button_width, button_height)
+            btn.clicked.connect(lambda checked, index=i: self.handle_button_click(index))
+            btn.show()
+            self.buttons.append(btn)
+
+    def toggle_task_completed(self, task, button_index, checked):
+        # Обновляем статус выполнения задачи
+        task['completed'] = checked
+
+        # Определяем категорию задачи, чтобы обновить в self.tasks_data
+        if task in self.tasks_data[str(button_index)]["important_tasks"]:
+            category = "important_tasks"
+        elif task in self.tasks_data[str(button_index)]["tasks_high_priority"]:
+            category = "tasks_high_priority"
+        elif task in self.tasks_data[str(button_index)]["additional_tasks"]:
+            category = "additional_tasks"
+        elif task in self.tasks_data[str(button_index)]["tasks_low_priority"]:
+            category = "tasks_low_priority"
+        else:
+            # Если задача не найдена в списках, выводим сообщение об ошибке
+            QMessageBox.warning(self, 'Ошибка', 'Не удалось найти задачу в списках.')
+            return
+
+        # Находим индекс задачи в соответствующем списке и обновляем ее статус
+        for i, t in enumerate(self.tasks_data[str(button_index)][category]):
+            if t['name'] == task['name']:
+                self.tasks_data[str(button_index)][category][i]['completed'] = checked
+                break
+
+        # Сохраняем обновленные данные в файл
+        self.save_tasks_to_file()
+    def handle_button_click(self, button_index):
+        self.clear_window(keep_main_buttons=True)
         self.setFixedSize(500, 700)
+
+
+        button_tasks = self.tasks_data.get(str(button_index), {
+            "important_tasks": [],
+            "additional_tasks": [],
+            "tasks_high_priority": [],
+            "tasks_low_priority": []
+        })
+
+
+        self.text_high = QtWidgets.QLabel("HIGH", self)
+        self.text_high.move(220, 50)
+        self.text_high.setStyleSheet("font-size: 15pt; color: #000000;")
+        self.text_high.show()
+
+        self.text_low = QtWidgets.QLabel("LOW", self)
+        self.text_low.move(220, 300)
+        self.text_low.setStyleSheet("font-size: 15pt; color: #000000;")
+        self.text_low.show()
+
+        if button_index in range(1, 8):
+            self.add_task_group(button_tasks["important_tasks"], 100, True, button_index)
+            self.add_task_group(button_tasks["tasks_high_priority"], 150, True, button_index)
+            self.add_task_group(button_tasks["additional_tasks"], 350, False, button_index)
+            self.add_task_group(button_tasks["tasks_low_priority"], 400, False, button_index)
+        else:
+            self.main_screen()
+
+        setup_ui_elements(self)
+
+    def main_screen(self, button_index='1'):
+        self.clear_window(keep_main_buttons=True)
+        self.setFixedSize(500, 700)
+        self.setup_main_buttons()
+
+        button_tasks = self.tasks_data.get(button_index, {
+            "important_tasks": [],
+            "additional_tasks": [],
+            "tasks_high_priority": [],
+            "tasks_low_priority": []
+        })
 
         self.text1 = QtWidgets.QLabel("HIGH", self)
         self.text1.move(220, 50)
         self.text1.setStyleSheet("font-size: 15pt; color: #000000;")
         self.text1.adjustSize()
 
-        self.add_task_group(self.important_tasks, 100, False)
-        self.add_task_group(self.tasks_high_priority, 150, True)
+        # Добавляем аргумент button_index в вызовы add_task_group
+        self.add_task_group(button_tasks["important_tasks"], 100, True, button_index)
+        self.add_task_group(button_tasks["tasks_high_priority"], 150, True, button_index)
 
         self.text2 = QtWidgets.QLabel("LOW", self)
         self.text2.move(220, 300)
         self.text2.setStyleSheet("font-size: 15pt; color: #000000;")
         self.text2.adjustSize()
-        self.add_task_group(self.additional_tasks, 350, True)
-        self.add_task_group(self.tasks_low_priority, 400, False)
+
+        self.add_task_group(button_tasks["additional_tasks"], 350, False, button_index)
+        self.add_task_group(button_tasks["tasks_low_priority"], 400, False, button_index)
 
         self.text1.show()
         self.text2.show()
-        self.weekend_label, self.weeken_dropdown = weekend_dropdown(self)
-
         setup_ui_elements(self)
 
     def main_page(self):
@@ -193,12 +268,12 @@ class MainWin(QMainWindow):
         setup_ui_elements(self)
 
     def on_background_image_changed(self):
+
         image_file = self.background_image_dropdown.currentText()
         image_path = f"{image_file}"
         if self.apply_background_image(image_path):
             self.current_background_image = image_path
             self.save_settings()
-
 
     def apply_font_size_from_input(self):
         font_size_str = self.font_size_input.text()
@@ -211,10 +286,11 @@ class MainWin(QMainWindow):
 
         if 5 <= font_size <= 32:
             self.current_font_size = font_size
-            self.apply_font_size_style()  # Apply font size to all relevant elements
+            self.apply_font_size_style()
             self.save_settings()
         else:
             QMessageBox.warning(self, 'Ошибка', 'Размер шрифта должен быть между 5 и 32.')
+
 
     def apply_background_image(self, image_path):
         pixmap = QPixmap(image_path)
@@ -233,90 +309,91 @@ class MainWin(QMainWindow):
         if hasattr(self, 'notes_text_edit'):
             self.notes_text_edit.setStyleSheet(font_size_style)
 
-    def add_task_group(self, tasks, y_start, is_important):
+    def add_task_group(self, tasks, y_start, is_important, button_index):
         for i, task in enumerate(tasks):
-            if task in ["Добавить важных дел", "Добавить дел"]:
-                btn = QtWidgets.QPushButton(task, self)
-                btn.setGeometry(25, y_start + i * 50, 450, 40)
-                checkbox = None
-                delete_btn = None
+            task_name = task['name']
+            btn = QtWidgets.QPushButton(task_name, self)
+            btn.setGeometry(25, y_start + i * 50, 450, 40)
+
+            if task_name in ["Добавить важных дел", "Добавить дел"]:
+                btn.setStyleSheet(
+                    "QPushButton { border-radius: 15px; background-color: white; color: #BBBBBB; border: 1px solid; "
+                    "border-color: #989898; font-size: 20px}")
+                if task_name == "Добавить важных дел":
+                    btn.clicked.connect(lambda _, b_index=button_index: self.add_important_task_input(b_index))
+                else:
+                    btn.clicked.connect(lambda _, b_index=button_index: self.add_additional_task_input(b_index))
             else:
-                btn = QtWidgets.QPushButton(task, self)
-                btn.setGeometry(25, y_start + i * 50, 450, 40)
+                task_completed = task.get('completed', False)
+                btn.setStyleSheet(
+                    "QPushButton { border-radius: 15px; background-color: white; color: black; border: 1px solid; "
+                    "border-color: #989898; font-size: 20px}")
 
                 checkbox = QtWidgets.QCheckBox(self)
+                checkbox.setChecked(task_completed)
                 checkbox.setGeometry(40, y_start + i * 50 + 10, 30, 30)
-
                 checkbox.setStyleSheet(
                     "QCheckBox::indicator { width: 25px; height: 25px; border-radius: 15px; border: 3px solid #989898;}"
                     "QCheckBox::indicator:checked { background-color: #808080;}"
                     "QCheckBox::indicator:unchecked { background-color: white;}"
                 )
+                checkbox.toggled.connect(
+                    lambda checked, t=task, b_index=button_index: self.toggle_task_completed(t, b_index, checked))
+                checkbox.show()
 
                 delete_btn = QtWidgets.QPushButton("☓", self)
                 delete_btn.setGeometry(430, y_start + i * 50 + 10, 25, 25)
                 delete_btn.setStyleSheet("background-color: #FF0000; color: white;")
-                delete_btn.clicked.connect(lambda _, t=task: self.delete_task(t, is_important))
-
-            if task in ["Добавить важных дел", "Добавить дел"]:
-                btn.setStyleSheet(
-                    "QPushButton { border-radius: 15px; background-color: white; color: #BBBBBB; border: 1px solid; "
-                    "border-color: #989898; font-size: 20px}")
-                if task == "Добавить важных дел":
-                    btn.clicked.connect(self.add_important_task_input)
-                elif task == "Добавить дел":
-                    btn.clicked.connect(self.add_additional_task_input)
-            else:
-                btn.setStyleSheet(
-                    "QPushButton { border-radius: 15px; background-color: white; color: black; border: 1px solid; "
-                    "border-color: #989898; font-size: 20px}")
-
-            btn.show()
-            if checkbox:
-                checkbox.show()
-            if delete_btn:
+                delete_btn.clicked.connect(
+                    lambda _, t=task, b_index=button_index: self.delete_task(t, b_index))
                 delete_btn.show()
 
-    def delete_task(self, task, is_important):
+            btn.show()
+
+    def delete_task(self, task, button_index):
         try:
-            if is_important:
-                if task in self.important_tasks:
-                    self.important_tasks.remove(task)
-                elif task in self.tasks_high_priority:
-                    self.tasks_high_priority.remove(task)
+            category = None
+            for cat in ["important_tasks", "tasks_high_priority", "additional_tasks", "tasks_low_priority"]:
+                for t in self.tasks_data[str(button_index)][cat]:
+                    if t['name'] == task['name']:
+                        category = cat
+                        break
+                if category:  # Если задача найдена, прервать внешний цикл
+                    break
+
+            if category:
+                self.tasks_data[str(button_index)][category] = [
+                    t for t in self.tasks_data[str(button_index)][category] if t['name'] != task['name']
+                ]
+                self.save_tasks_to_file()
+                self.handle_button_click(button_index)  # Обновляем отображение для соответствующего дня
             else:
-                self.tasks_low_priority.remove(task)
-            self.save_tasks_to_file()
+                QMessageBox.warning(self, 'Ошибка', 'Задача не найдена для удаления.')
 
-            self.clear_window()
-            self.main_screen()
-        except ValueError as e:
-            print(f"Error deleting task: {e}")
+        except Exception as e:
+            QMessageBox.warning(self, 'Ошибка', f'Ошибка при удалении задачи: {e}')
 
-    def add_important_task_input(self):
-        text, ok = QtWidgets.QInputDialog.getText(self, 'Добавить важных дел', 'Добавить задачу:')
+    def add_important_task_input(self, button_index):
+        text, ok = QInputDialog.getText(self, 'Добавить важных дел', 'Введите название задачи:')
         if ok and text:
-            if len(self.tasks_high_priority) < 3:
-                self.tasks_high_priority.append(text)
-            else:
-                QMessageBox.information(self, 'Сообщение', 'Вы уже добавили три задачи!')
+            new_task = {"name": text, "completed": False}
+            self.tasks_data[str(button_index)]["tasks_high_priority"].append(new_task)
             self.save_tasks_to_file()
-            self.main_screen()
+            self.handle_button_click(button_index)
 
-    def add_additional_task_input(self):
-        text, ok = QtWidgets.QInputDialog.getText(self, 'Добавить дел', 'Добавить задачу:')
+    def add_additional_task_input(self, button_index):
+        text, ok = QInputDialog.getText(self, 'Добавить дел', 'Введите название задачи:')
         if ok and text:
-            if len(self.tasks_low_priority) < 3:
-                self.tasks_low_priority.append(text)
-            else:
-                QMessageBox.information(self, 'Сообщение', 'Вы уже добавили три задачи!')
-
+            new_task = {"name": text, "completed": False}
+            self.tasks_data[str(button_index)]["additional_tasks"].append(new_task)
             self.save_tasks_to_file()
-            self.main_screen()
+            self.handle_button_click(button_index)
 
-    def clear_window(self):
+    def clear_window(self, keep_main_buttons=False):
+        widgets_to_keep = self.buttons if keep_main_buttons else []
         for widget in self.findChildren(QtWidgets.QWidget):
-            widget.deleteLater()
+            if widget not in widgets_to_keep:
+                widget.deleteLater()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
@@ -340,6 +417,6 @@ def run_app():
     except Exception as e:
         print(f"Произошла непредвиденная ошибка: {e}")
 
+
 if __name__ == '__main__':
     run_app()
-
